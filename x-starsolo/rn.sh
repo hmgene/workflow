017cd25b85fe4c81e2bd633b52679f6e7c96029e
hmgene <human.gim@gmail.com>
2024-09-30 09:49:34 -0400

    starsolo

---
+++ x-starsolo/rn.sh
@@ -0,0 +1,4 @@
+
+
+in=gs://millerlab-bucket/hyunmin/medulloblastoma/riemondy_2022/bam_orig/SRR12354564/Foreman_753_03142019_1_possorted_genome_bam
+in=gs://millerlab-bucket/hyunmin/medulloblastoma/riemondy_2022/starsolo/input.tsv
